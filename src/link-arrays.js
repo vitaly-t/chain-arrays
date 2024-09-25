922528acaf399f044dbeaf01c043b5f5b1229071
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2024-09-26 00:39:52 +0100

    add js

---
+++ src/link-arrays.js
@@ -0,0 +1,45 @@
+/**
+ * Logically concatenates arrays (links them), into an iterable.
+ */
+function linkArrays(...arr) {
+    return {
+        [Symbol.iterator]() {
+            let i = 0, k = -1, a = [];
+            return {
+                next() {
+                    while (i === a.length) {
+                        if (++k === arr.length) {
+                            return { done: true, value: undefined };
+                        }
+                        a = arr[k];
+                        i = 0;
+                    }
+                    return { value: a[i++], done: false };
+                }
+            };
+        }
+    };
+}
+
+/**
+ * Logically concatenates arrays (links them), into a reversed iterable.
+ */
+function linkArraysReverse(...arr) {
+    return {
+        [Symbol.iterator]() {
+            let i = -1, k = arr.length, a;
+            return {
+                next() {
+                    while (i < 0) {
+                        if (--k < 0) {
+                            return { done: true, value: undefined };
+                        }
+                        a = arr[k];
+                        i = a.length - 1;
+                    }
+                    return { value: a[i--], done: false };
+                }
+            };
+        }
+    };
+}
