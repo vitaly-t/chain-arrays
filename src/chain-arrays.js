780c2166f80b2c33dd8eb03b3082375030390dfc
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2024-09-26 15:56:31 +0100

    js update

--- src/chain-arrays.js
+++ src/chain-arrays.js
@@ -1,7 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chainArrays = chainArrays;
-exports.chainArraysReverse = chainArraysReverse;
 /**
  * Logically concatenates arrays (chains them), into an iterable.
  */
