682148172a4da024ef907393737fb86b92ffc1a5
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2024-09-27 22:42:38 +0100

    improve the test

--- src/test.ts
+++ src/test.ts
@@ -4,12 +4,17 @@ const a = [1, 2];
 const b = [3, 4];
 const c = [5, 6];
 
-for (const val of chainArrays(a, b, c)) {
+const i = chainArrays(a, b, c);
+const r = chainArraysReverse(a, b, c);
+
+for (const val of i) {
     console.log(val); //=> 1, 2, 3, 4, 5, 6
 }
+console.log(`first: ${i.at(0)}, last: ${i.at(i.length - 1)}`); //=> first: 1, last: 6
 
 console.log('Reverse iteration:');
 
-for (const val of chainArraysReverse(a, b, c)) {
+for (const val of r) {
     console.log(val); //=> 6, 5, 4, 3, 2, 1
 }
+console.log(`first: ${r.at(0)}, last: ${r.at(r.length - 1)}`); //=> first: 6, last: 1
