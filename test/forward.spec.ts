8b77a01d1ac374376c31a2b5f9944e6714fe02d3
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2024-09-30 20:57:15 +0100

    adding tests

--- test/forward.spec.ts
+++ test/forward.spec.ts
@@ -6,5 +6,14 @@ describe('forward', () => {
         it('must produce all elements', () => {
             expect([...chainArrays([1, 2], [3, 4], [5, 6])]).to.eql([1, 2, 3, 4, 5, 6]);
         });
+        it('must work empty', () => {
+            expect([...chainArrays()]).to.eql([]);
+        });
+        it('must skip empty arrays', () => {
+            expect([...chainArrays([], [3, 4], [5, 6])]).to.eql([3, 4, 5, 6]);
+            expect([...chainArrays([], [3, 4], [])]).to.eql([3, 4]);
+            expect([...chainArrays([1, 2], [], [5, 6])]).to.eql([1, 2, 5, 6]);
+            expect([...chainArrays([], [], [])]).to.eql([]);
+        })
     });
 });
